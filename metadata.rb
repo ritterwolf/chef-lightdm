2e7b3a73d227e95f669bca66bf634e3a42cfcacc
Lyle Dietz <ritterwolf@gmail.com>
2014-08-05 11:25:32 +1000

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name              'lightdm'
+maintainer        'Lyle Dietz'
+maintainer_email  'ritterwolf@gmail.com'
+license           'Apache 2.0'
+description       'Set up lightdm'
+#long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version           '0.0.1'
+
+supports 'ubuntu', '= 14.04'
